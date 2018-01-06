f0b764e666e89055f1340b25ca6efb1ed5fb3fd3
Giang Nam <namle@kinssolutions.net>
2018-01-06 15:57:19 +0700

    asd

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var ss = require('socket.io-stream');
+var path = require('path');
+var fs = require('fs');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(8085, function() {
+io.on('connection', function (socket) {
+    ss(socket).on('file', function(stream, data) {
+        var filename = "images/" + path.basename(data.name);
+        stream.pipe(fs.createWriteStream(filename));
+    });
+});
+
+server.listen(8085, function() {
     console.log('Example app listening on port 8085!');
-})
+});
